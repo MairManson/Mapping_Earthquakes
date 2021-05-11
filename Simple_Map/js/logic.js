6ccbb205794a686f24a928244d76f434dd3e0c15
MairManson <marilyn.alicia@gmail.com>
2021-05-11 17:12:23 -0400

    Add index.html, logic.js, and style.css

---
+++ Simple_Map/js/logic.js
@@ -0,0 +1,17 @@
+// Add console.log to check logic.js is being accessed in the Chrome console
+console.log("working");
+
+// Create the map object with a center and zoom level -- approximate geographical center of US
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// Create the tile layer for map background
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
